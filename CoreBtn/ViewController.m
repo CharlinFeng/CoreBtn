5a6dfacecfcd20f2f76ffbad0335db11565b73fe
Charlin_冯成林 <muxi@muxideiMac.local>
2015-03-02 23:47:57 +0800

    CoreBtn

--- CoreBtn/ViewController.m
+++ CoreBtn/ViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "ViewController.h"
+#import "CoreStatusBtn.h"
 
 @interface ViewController ()
+
+@property (nonatomic,strong) CoreStatusBtn *btn;
+
+@property (weak, nonatomic) IBOutlet CoreStatusBtn *myBtn;
+
 
 @end
 
@@ -16,12 +22,46 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    //代码
+    
+    CoreStatusBtn *btn=[[CoreStatusBtn alloc] initWithFrame:CGRectMake(50, 110, 160, 40)];
+    btn.msg=@"登陆中";
+    
+    //设置文字
+    [btn setTitle:@"我是按钮" forState:UIControlStateNormal];
+    
+    btn.backgroundColorForNormal=[UIColor brownColor];
+    btn.backgroundColorForHighlighted=[UIColor redColor];
+    btn.backgroundColorForSelected=[UIColor yellowColor];
+    self.btn=btn;
+    
+    //添加事件
+    [btn addTarget:self action:@selector(btnClick:) forControlEvents:UIControlEventTouchUpInside];
+   
+    [self.view addSubview:btn];
+    
+
+    //nib
+    self.myBtn.backgroundColorForNormal=rgb(121, 36, 98);
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+-(IBAction)btnClick:(CoreStatusBtn *)btn{
+    
+    NSLog(@"事件来了：%@",btn);
+    
+    btn.status=CoreStatusBtnStatusProgress;
+
+    //子线程测试
+    dispatch_queue_t queue=dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0f * NSEC_PER_SEC)), queue, ^{
+        NSLog(@"当前线程:%@",[NSThread currentThread]);
+        btn.status=CoreStatusBtnStatusFalse;
+    });
 }
+
+
 
 @end
