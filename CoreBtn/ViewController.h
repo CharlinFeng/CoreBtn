af588ab946b21c3ee8010fcc404a38ed57cfba7b
Charlin_冯成林 <muxi@muxideiMac.local>
2015-03-02 21:13:00 +0800

    Initial Commit

---
+++ CoreBtn/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreBtn
+//
+//  Created by 沐汐 on 15-3-2.
+//  Copyright (c) 2015年 沐汐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CoreBtnTests/CoreBtnTests.m
@@ -0,0 +1,40 @@
+//
+//  CoreBtnTests.m
+//  CoreBtnTests
+//
+//  Created by 沐汐 on 15-3-2.
+//  Copyright (c) 2015年 沐汐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface CoreBtnTests : XCTestCase
+
+@end
+
+@implementation CoreBtnTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
